320ea148c607d32e28a92fa8dba7434b495cfeb8
Leirbag <gomesrdg34@gmail.com>
2023-11-11 13:26:39 -0300

    baby

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'custom-color': '#fffafa',
+      },
+    },
+  },
+  plugins: [],
+}
+
